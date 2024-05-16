ea21e0a59fc9f5eaaa212c6727e1da48f6d3cb69
Koji Adriano <iisparkplugiiog@gmail.com>
2024-05-16 14:54:28 +0800

    Initial commit

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginComponent = () => {
+  return (
+    <div className='sm:container text-center'>
+      <h1>Login Page</h1>
+    </div>
+  )
+}
+
+export default LoginComponent;
