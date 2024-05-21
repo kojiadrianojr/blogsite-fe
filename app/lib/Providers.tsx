9ac6eb345862c648ebb3df33776647a1f1722149
Koji Adriano <iisparkplugiiog@gmail.com>
2024-05-21 22:05:18 +0800

    feature: Create 'Provders' that stores context provider

---
+++ app/lib/Providers.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AuthContextProvider } from "./auth/AuthContextProvider";
+import { DataContextProvider } from "./data/DataContextProvider";
+import { ToastContainerProvider } from "../features/Toasts";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <AuthContextProvider>
+      <DataContextProvider>
+        <ToastContainerProvider>{children}</ToastContainerProvider>
+      </DataContextProvider>
+    </AuthContextProvider>
+  );
+}
